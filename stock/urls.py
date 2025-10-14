92fb5e223493500d5b11e606e9ca19c8a4ca6358
vndevv <vndevv@proton.me>
2025-10-15 02:07:39 +0300

    django lab2 p5

--- stock/urls.py
+++ stock/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from stock.views import stock_list
+from stock.views import stock_list, stock_detail, stock_buy
 
 
 urlpatterns = [
-    path('list/', stock_list, name='list')
+    path('list/', stock_list, name='list'),
+    path('detail/<int:pk>/', stock_detail, name='detail'),
+    path('buy/<int:pk>/', stock_buy, name='buy')
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-from stock.models import Stock
+from stock.models import Stock, AccountCurrency, AccountStock
+from stock.forms import BuySellForm
 
 
 def stock_list(request):
@@ -9,3 +10,53 @@ def stock_list(request):
         'stocks': stocks,
     }
     return render(request, 'stocks.html', context)
+
+
+def stock_detail(request, pk):
+    stock = get_object_or_404(Stock, pk=pk)
+    context = {
+        'stock': stock,
+        'form': BuySellForm(initial={'price': stock.get_random_price()})
+    }
+    return render(request, 'stock.html', context)
+
+
+def stock_buy(request, pk):
+    if request.method != "POST":
+        return redirect('stock:detail', pk=pk)
+
+    stock = get_object_or_404(Stock, pk=pk)
+    form = BuySellForm(request.POST)
+
+    if form.is_valid():
+        amount = form.cleaned_data['amount']
+        price = form.cleaned_data['price']
+        buy_cost = price * amount
+
+        acc_stock, created = AccountStock.objects.get_or_create(account=request.user.account, stock=stock,
+                                                                defaults={'average_buy_cost': 0, 'amount': 0})
+        current_cost = acc_stock.average_buy_cost * acc_stock.amount
+
+        total_cost = current_cost + buy_cost
+        total_amount = acc_stock.amount + amount
+
+        acc_stock.amount = total_amount
+        acc_stock.average_buy_cost = total_cost / total_amount
+
+        acc_currency, created = AccountCurrency.objects.get_or_create(account=request.user.account, currency=stock.currency,
+                                                                      defaults={'amount': 0})
+
+        if acc_currency.amount < buy_cost:
+            form.add_error(None, f'На счёте недостаточно средств в валюте {stock.currency.sign}')
+        else:
+            acc_currency.amount = acc_currency.amount - buy_cost
+            acc_stock.save()
+            acc_currency.save()
+            return redirect('stock:list')
+
+    context = {
+        'stock': get_object_or_404(Stock, pk=pk),
+        'form': form
+    }
+
+    return render(request, 'stock.html', context)
